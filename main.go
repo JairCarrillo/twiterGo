4a61465f33b7adcb37bb172b3d322d943c8ce8f7
jaircarrillo <jcarrillo@sistemasenlinea.com.co>
2024-05-21 10:23:48 -0500

    nuevo commit

--- main.go
+++ main.go
@@ -14,15 +14,19 @@ import (
 	lambda "github.com/aws/aws-lambda-go/lambda"
 )
 
+// Función principal que inicia la ejecución de la función Lambda
 func main() {
 	lambda.Start(EjecutoLambda)
 }
 
+// Función que se ejecuta cuando se invoca la función Lambda
 func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	var res *events.APIGatewayProxyResponse
 
+	// Inicializa la configuración de AWS
 	awsgo.InicializoAWS()
 
+	// Verifica si los parámetros necesarios están presentes
 	if !ValidoParametros() {
 		res = &events.APIGatewayProxyResponse{
 			StatusCode: 400,
@@ -34,6 +38,7 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (
 		return res, nil
 	}
 
+	// Obtiene el secreto de AWS Secrets Manager
 	SecretModel, err := secretmanager.GetSecret(os.Getenv("SecretName"))
 	if err != nil {
 		res = &events.APIGatewayProxyResponse{
@@ -46,8 +51,10 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (
 		return res, nil
 	}
 
+	// Modifica el path del request para obtener la ruta real
 	path := strings.Replace(request.PathParameters["twittergo"], os.Getenv("UrlPrefix"), "", -1)
 
+	// Almacena variables en el contexto
 	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("path"), path)
 	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("method"), request.HTTPMethod)
 	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("user"), SecretModel.Username)
@@ -58,8 +65,7 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (
 	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("body"), request.Body)
 	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("bucketName"), os.Getenv("BucketName"))
 
-	//Chequeo conexion a la base de datos
-
+	// Verifica la conexión a la base de datos
 	err = bd.ConectarBD(awsgo.Ctx)
 	if err != nil {
 		res = &events.APIGatewayProxyResponse{
@@ -72,6 +78,7 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (
 		return res, nil
 	}
 
+	// Llama a los manejadores para procesar el request
 	resAPI := handlers.Manejadores(awsgo.Ctx, request)
 	if resAPI.CustomResp == nil {
 		res = &events.APIGatewayProxyResponse{
@@ -85,9 +92,9 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (
 	} else {
 		return resAPI.CustomResp, nil
 	}
-
 }
 
+// Función que valida la presencia de las variables de entorno necesarias
 func ValidoParametros() bool {
 	_, traeParametro := os.LookupEnv("SecretName")
 	if !traeParametro {
